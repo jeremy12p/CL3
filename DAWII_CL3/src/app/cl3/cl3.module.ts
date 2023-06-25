d3b9b9c75fea10b1478ee3a87519b80839771e46
jeremy12p <chparattori@outlook.com>
2023-06-25 18:10:27 -0500

    se elimino componentes de api rest 2

--- DAWII_CL3/src/app/cl3/cl3.module.ts
+++ DAWII_CL3/src/app/cl3/cl3.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalificacionComponent } from './paginas/calificacion/calificacion.component';
 import { Apirest1Component } from './paginas/apirest1/apirest1.component';
-import { Apirest2Component } from './paginas/apirest2/apirest2.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { MaterialModule } from '../angularmaterial/material.module';
 
@@ -10,8 +9,7 @@ import { MaterialModule } from '../angularmaterial/material.module';
 @NgModule({
   declarations: [
     CalificacionComponent,
-    Apirest1Component,
-    Apirest2Component
+    Apirest1Component
   ],
   imports: [
     AppRoutingModule,
