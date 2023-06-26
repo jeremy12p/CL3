c390e7648ee3fec3881a357e9e8377addbb3bf25
lino1202 <pier-lino@hotmail.com>
2023-06-26 00:59:29 -0500

    detalle.component

--- DAWII_CL3/src/app/cl3/paginas/apitest/detalle/detalle.component.ts
+++ DAWII_CL3/src/app/cl3/paginas/apitest/detalle/detalle.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.component.html',
   styleUrls: ['./detalle.component.sass']
 })
-export class DetalleComponent {
 
+export class DetalleComponent implements OnInit {
+
+  usuarioId: string | null = "";
+
+  constructor(private router: ActivatedRoute){
+
+  }
+
+  ngOnInit(): void {
+    this.usuarioId = this.router.snapshot.paramMap.get('id');
+  }
+ 
 }
